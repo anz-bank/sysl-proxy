651d737c0dd828ebe75b7b41290ad206c5ea7955
joshcarp <joshuacarpeggiani@gmail.com>
2020-10-06 11:46:10 +1100

    Simplify proxy and add comprehensive tests

--- go.mod
+++ go.mod
@@ -3,14 +3,9 @@ module github.com/joshcarp/sysl-proxy
 go 1.14
 
 require (
-	cloud.google.com/go v0.66.0 // indirect
-	cloud.google.com/go/storage v1.11.0 // indirect
-	github.com/anz-bank/sysl v0.208.0
-	github.com/joshcarp/gop v0.0.0-20200916004237-9af9bb2a875a
-	github.com/spf13/afero v1.4.0
-	golang.org/x/sys v0.0.0-20200916030750-2334cc1a136f // indirect
-	golang.org/x/tools v0.0.0-20200915201639-f4cefd1cb5ba // indirect
-	google.golang.org/api v0.32.0 // indirect
-	google.golang.org/genproto v0.0.0-20200915202801-9f80d0600517 // indirect
+	github.com/anz-bank/sysl v0.228.0
+	github.com/joshcarp/gop v0.4.1-0.20201006004131-742073ac3697
+	github.com/stretchr/testify v1.6.1
+	golang.org/x/sys v0.0.0-20200916084744-dbad9cb7cb7a // indirect
 	google.golang.org/protobuf v1.25.0
 )
--- retriever.go
+++ retriever.go
@@ -2,35 +2,28 @@ package sysl_proxy
 
 import (
 	"github.com/anz-bank/sysl/pkg/parse"
-	"github.com/anz-bank/sysl/pkg/sysl"
 	"github.com/joshcarp/gop/gop"
 	"google.golang.org/protobuf/encoding/protojson"
 )
 
-type Retriever struct {
-	primary      gop.Retriever
-	secondary    gop.Retriever
+type Processor struct{
+	retr gop.Retriever
+	parser *parse.Parser
 }
 
-func New(primary, secondary gop.Retriever) Retriever {
-	return Retriever{primary: primary, secondary: secondary}
+func NewProcessor(retr gop.Retriever)Processor{
+	return Processor{retr:retr, parser: parse.NewParser()}
 }
 
-func (a Retriever) Retrieve(resource string) ([]byte, bool, error) {
-	var res []byte
-	var cached bool
-	var err error
-	var m *sysl.Module
-	res, cached, err = a.primary.Retrieve(resource)
-	if err == nil {
-		return res, cached, err
+func (a Processor) Retrieve(resource string) ([]byte, bool, error) {
+	m, err := a.parser.Parse(resource, a.retr)
+	if err != nil {
+		return nil, false, err
 	}
-	p := parse.NewParser()
-	p.SetVersioned()
-	m, err = p.Parse(resource, a.secondary)
-	res, err = protojson.Marshal(m)
+	res, err := protojson.Marshal(m)
 	if err != nil {
 		return res, false, err
 	}
-	return res, cached, nil
+	return res, false, nil
 }
+
